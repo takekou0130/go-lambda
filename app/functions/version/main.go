0c780979fc99345db9a7d7c2c217ff33498beff7
Koya Takei <takekou0130@gmail.com>
2024-09-28 19:24:54 +0900

    update runtime image

--- app/functions/version/main.go
+++ app/functions/version/main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"runtime"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
+var jst = time.FixedZone("Asia/Tokyo", 9*60*60)
+
+type Request struct {
+	DryRun bool `json:"dry_run"`
+}
+
+type Response struct {
+	Message   string    `json:"message"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
 func main() {
-	fmt.Println(runtime.Version())
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, req Request) (Response, error) {
+	fmt.Println("start version function")
+	fmt.Println(req)
+	version := runtime.Version()
+	fmt.Println(version)
+	return Response{version, time.Now().In(jst)}, nil
 }
--- app/go.mod
+++ app/go.mod
@@ -1,3 +1,5 @@
 module go-lambda
 
 go 1.23.1
+
+require github.com/aws/aws-lambda-go v1.47.0
